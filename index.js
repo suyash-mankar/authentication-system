19247a244d541dc5e5e633e9284f029fb442d82c
Suyash Mankar <suyashmankar@gmail.com>
2022-11-21 12:34:06 +0530

    create homepage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 5000;
 const expressLayouts = require("express-ejs-layouts");
 
 app.use(expressLayouts);
